7e3f53910c86c056243ed835c61cbb0a50033cff
septiandwica <septiandwica03@gmail.com>
2025-07-10 15:24:10 +0700

    commit

--- server/middleware/adminAuth.js
+++
@@ -1,9 +0,0 @@
-const adminAuth = (req, res, next) => {
-  if (req.user.role !== "admin") {
-    return res.status(403).json({ error: "Forbidden" });
-  }
-
-  next();
-};
-
-module.exports = adminAuth;
